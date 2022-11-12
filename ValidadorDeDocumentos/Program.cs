36dab8c1290daa48584d60b6ded46b312661813d
Rafael Menezes <rafa.rcm@hotmail.com>
2022-11-12 16:06:37 -0300

    adicionando validarCnpj e validarTitulo

--- ValidadorDeDocumentos/Program.cs
+++ ValidadorDeDocumentos/Program.cs
@@ -14,6 +14,43 @@ namespace ValidadorDeDocumentos
             ValidarCPF(cpf1);
             ValidarCPF(cpf2);
             ValidarCPF(cpf3);
+
+            string cnpj1 = "51241758000152";
+            string cnpj2 = "14128481000127";
+
+            ValidarCNPJ(cnpj1);
+            ValidarCNPJ(cnpj2);
+
+            string titulo1 = "041372570132";
+            string titulo2 = "774387480132";
+
+            ValidarTitulo(titulo1);
+            ValidarTitulo(titulo2);
+
+        }
+
+        private static void ValidarTitulo(string titulo)
+        {
+            if (new TituloEleitoralValidator().IsValid(titulo))
+            {
+                Debug.WriteLine("Título válido: " + titulo);
+            }
+            else
+            {
+                Debug.WriteLine("Título inválido: " + titulo);
+            }
+        }
+
+        private static void ValidarCNPJ(string cnpj)
+        {
+            if (new CNPJValidator().IsValid(cnpj))
+            {
+                Debug.WriteLine("Cnpj válido: " + cnpj);
+            }
+            else
+            {
+                Debug.WriteLine("Cnpj inválido: " + cnpj);
+            }
         }
 
         private static void ValidarCPF(string cpf)
