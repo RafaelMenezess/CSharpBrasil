fb41cb043f2978cb929d8fd22a6492a12e666edf
Rafael Menezes <rafa.rcm@hotmail.com>
2022-11-16 21:22:34 -0300

    NumerosPorExtenso

---
+++ NumerosPorExtenso/Program.cs
@@ -0,0 +1,26 @@
+﻿using Caelum.Stella.CSharp.Inwords;
+using System.Diagnostics;
+
+namespace NumerosPorExtenso
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 75;
+            string extenso = new Numero(valor).Extenso();
+            Debug.WriteLine(extenso);
+
+            valor = 1532897;
+            extenso = new Numero(valor).Extenso();
+            Debug.WriteLine(extenso);
+
+            string extensoBrl = new MoedaBRL(valor).Extenso();
+            Debug.WriteLine(extensoBrl);
+
+            valor = 1532897.89;
+            extensoBrl = new MoedaBRL(valor).Extenso();
+            Debug.WriteLine(extensoBrl);
+        }
+    }
+}
