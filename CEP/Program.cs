bfb77889e052aeb7f33cfc7215c6f88849c53f8b
Rafael Menezes <rafa.rcm@hotmail.com>
2022-11-16 22:21:56 -0300

    Consultando Cep via Caelum

--- CEP/Program.cs
+++ CEP/Program.cs
@@ -1,17 +1,35 @@
-﻿using System.Diagnostics;
+﻿using Caelum.Stella.CSharp.Http;
+using System.Diagnostics;
 using System.Net.Http;
 
 namespace CEP
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             string cep = "01001000";
-            string url = "https://viacep.com.br/ws/" + cep + "/json/";
+            string result = GetEndereco(cep);
 
-            string result = new HttpClient().GetStringAsync(url).Result;
             Debug.WriteLine(result);
+
+            ViaCEP viaCEP = new ViaCEP();
+            string enderecoJson = viaCEP.GetEnderecoJson(cep);
+            Debug.WriteLine(enderecoJson);
+
+            string enderecoXml = viaCEP.GetEnderecoXml(cep);
+            Debug.WriteLine(enderecoXml);
+
+            var task = viaCEP.GetEnderecoJsonAsync(cep);
+            Debug.WriteLine(task.Result);
+
+            var endereco = viaCEP.GetEndereco(cep);
+            Debug.WriteLine(string.Format($"Logradouro: {0}, Bairro: {1}"));
+        }
+
+        private static string GetEndereco(string cep)
+        {
+            return "https://viacep.com.br/ws/" + cep + "/json/";
         }
     }
 }
